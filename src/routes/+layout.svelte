1150b0dd0ecc9ecf970b9f744ea8719f848f06f2
Artemiy <artemii290907@gmail.com>
2023-07-02 01:24:45 +0300

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
     <a href="/" class="nav-logo" title="Back to Homepage">Artemiy Marinin</a>
     <div class="nav-links">
       {#each nav as link}
-      <a href={link.href} class="link">{link.title}</a>
+        {#if link.isShow}
+          <a href={link.href} class="link">{link.title}</a>
+        {/if}
       {/each}
     </div>
   </div>
@@ -25,27 +27,32 @@
   const nav = [
     {
       title: "Обо мне",
-      href: '/about'
+      href: '/about',
+      isShow: true
     },
     
     {
       title: "Блог",
-      href: '/blog'
+      href: '/blog',
+      isShow: true
     },
 
     {
       title: "Портфолио",
-      href: '/project'
+      href: '/project',
+      isShow: true
     },
 
     {
       title: "Контакты",
-      href: '/contact'
+      href: '/contact',
+      isShow: true
     },
 
     {
       title: "temp",
-      href: '/temp'
+      href: '/temp',
+      isShow: true
     }
   ]
 </script>
